4270f5b7cfd65accfad508dc5600a0c55a8f7f7e
ashishpolaki <ashishektha@gmail.com>
2025-09-05 12:17:24 +0530

    Added ENABLE_DEBUG in scripting defines

--- Assets/Scripts/Utils/DebugUtils.cs
+++ Assets/Scripts/Utils/DebugUtils.cs
@@ -6,34 +6,34 @@ namespace BeachHero
     {
         public static void Log(string message, Object context = null)
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.Log(message, context);
 #endif
         }
         public static void LogWarning(string message, Object context = null)
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.LogWarning(message, context);
 #endif
         }
 
         public static void LogError(string message, Object context = null)
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.LogError(message, context);
 #endif
         }
 
         public static void LogException(System.Exception exception, Object context = null)
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.LogException(exception, context);
 #endif
         }
 
         public static void LogFormat(string format, params object[] args)
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.LogFormat(format, args);
 #endif
         }
@@ -41,7 +41,7 @@ namespace BeachHero
         //Debug break
         public static void Break()
         {
-#if DEBUG
+#if ENABLE_DEBUG
             Debug.Break();
 #endif
         }
